c934e2996eeefc3cc944d029bb7cd182ed39aa75
Amitav Mitra <amitav.mitra@cern.ch>
2021-07-26 18:46:18 -0400

    miscellaneous findings

--- src/tool/Launcher_commands.cc
+++ src/tool/Launcher_commands.cc
@@ -92,10 +92,18 @@ CommandReturn::status Launcher::Echo(std::vector<std::string> strArg,
   for(size_t iArg = 0; iArg < strArg.size();iArg++)
     {
       //printf("%s ",strArg[iArg].c_str());
-      Print(Level::DEBUG, "%s ",strArg[iArg].c_str());
+      Print(Level::INFO, "%s ",strArg[iArg].c_str());
       /*
-          If added to Level::INFO and cout, then only BUTool's Print calls work
-          If another AddPreamble is added to ApolloSMDevice's on Level::INFO and cout, then there will be two separate preambles
+          If added to Level::INFO and cout, then only BUTool's Print calls work.
+          If added to Level::DEBUG, then nothing works.
+          If another AddPreamble is added to ApolloSMDevice's Level::INFO and cout, then there will be two separate preambles:
+            >echo $
+             2021-07-26 22:25:45-UTC [echo] $
+            >read SM_INFO.GIT_HASH_3
+             2021-07-26 22:26:28-UTC [SMDevice ctor]      SM_INFO.GIT_HASH_3: 0x4306F4DB
+          
+          Preambles differ for the same stream bc is tied to the device
+          But why does the adding a preamble to Launcher break ApolloSMDevice's printing?
       */
     }
   //printf("\n");
