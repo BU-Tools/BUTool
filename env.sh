31681e62d1596455be3de63c1e9e238a1c30d8e2
Alp <aakpinar@bu.edu>
2022-07-12 10:13:03 -0400

    Update the Makefile to latest v2.0 version, check plugins in env.sh

--- env.sh
+++ env.sh
@@ -3,12 +3,21 @@ export PATH=$PWD/bin/tool/:$PATH
 
 #setup LD_LIBRARY_PATHs for BUTool and any plugins
 #must be source AFTER build finishes
+
+# Add BUTool's lib/ directory to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
+
+# Add the plugin libraries to the LD_LIBRARY_PATH
+
 #plugin_paths=$(find $PWD/plugins/* -type d | xargs)
 plugin_paths=$(ls -1 -d $PWD/plugins/* | xargs)
-export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
+
 for path in $plugin_paths
 do
-    export LD_LIBRARY_PATH=$path/lib:$LD_LIBRARY_PATH
+    # Check if this is a directory
+    if [ -d $path  ]; then
+        export LD_LIBRARY_PATH=$path/lib:$LD_LIBRARY_PATH
+    fi
 done
   
 # Add BUException submodule to the LD_LIBRARY_PATH
